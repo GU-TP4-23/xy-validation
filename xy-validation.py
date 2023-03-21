33d72a8b5f84705cca285708493e6424361eccd0
Giorgio Ciacchella <2481024C@student.gla.ac.uk>
2023-03-21 16:53:42 +0000

    add adjustment loop
    implement adjustment loop: T6 -> T5
    [header new_x new_y]
    
    add adjustment confirmation message ('a'), mocking the CV decision
    and its  through a random.choice() coin-flip and
    hard-coded x,y correction values

--- xy-validation.py
+++ xy-validation.py
@@ -3,14 +3,17 @@ import struct
 import time
 import rasp_opencv
 
+import random
+
 BUS_NUM = 1
 XY_ADDR = 0x57
-header_mv = 0x61
-header_ok = 0x76
+header_mv = '\x61'
+header_ok = '\x76'
 
 def handler(gpio, level, tick):
 	global pi
 	print("interrupt received")
+	time.sleep(0.5);
 	(count, data) = pi.i2c_read_device(xy, 8)
 	print(f"read {count} bytes of raw data:\t", data)
 	x = struct.unpack_from('f', data, 0)
@@ -21,7 +24,7 @@ def handler(gpio, level, tick):
 	
 	"""
 	CV stuff
-	"""
+	
 	# used to track progress
 	team = 0
 	component = 0
@@ -29,11 +32,25 @@ def handler(gpio, level, tick):
 	# case when it is in position
 	if (new_x is None and new_y is None):
 		pass
-	
+	"""
+
 	time.sleep(5)
 	
-	print("writing confirmation...")
-	pi.i2c_write_byte(xy, header_ok)
+	if random.choice([True, False]):
+		print("writing confirmation...")
+		msg_ok = ord(header_ok)
+		pi.i2c_write_byte(xy, msg_ok)
+	else:
+		print("writing adjustment...")
+		new_x = 20000.0
+		new_y = 40000.0
+		msg_adj = bytearray(struct.pack(
+			"<cff",
+			bytes(header_mv, 'ascii'),
+			new_x,
+			new_y
+		))
+		pi.i2c_write_device(xy, msg_adj)
 
 if __name__ == "__main__":
 	print("initialising GPIO")
